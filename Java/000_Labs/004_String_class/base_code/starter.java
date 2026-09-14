f740af2e6ee905cf3308554b1c5dacdba44a542d
IrisVoongonUnicorns <irisvoong@gmail.com>
2026-09-14 19:00:50 +0000

    update

--- Java/000_Labs/004_String_class/base_code/starter.java
+++ Java/000_Labs/004_String_class/base_code/starter.java
@@ -9,6 +9,9 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		String happy = ("Happy birthday to you");
+		String birthday = ("Happy birthday dear ");
+		String princess = ("idk"); 
+		System.out.println() 
 	}
 }
