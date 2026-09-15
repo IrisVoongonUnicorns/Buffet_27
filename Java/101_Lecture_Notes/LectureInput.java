6639c3393df834c766d00d6763d77fe1eb6f6c1d
IrisVoongonUnicorns <irisvoong@gmail.com>
2026-09-15 17:59:12 +0000

    update

--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -1,9 +1,27 @@
 /*
     Lecture note example - Input!!
 */
+import java.until.Scanner
 
 class LectureInput{
     public static void main(String args[]) {
-        // Your Code Goes here!
+        System.out.println ("Welcome to Yunju Heeroz");
+        Here's our menu:
+        System.out.print("1. plum Cucumber stick - ");
+        double item1price = $2.47;
+        System.out.println(item1Price);
+
+        System.out.println("2. galbi - ");
+        double item2price = $34.92;
+        System.out.println(item2Price);
+
+        System.out.println("3. tiramisu mini circle slice - ");
+        double item3price = $19.65;
+        System.out.println(item3Price);
+
+        System.out.println("How many plum Cucumber sticks would you like?");
+        Scanner sc = new Scanner(System.in);
+
+
 	}
 }
