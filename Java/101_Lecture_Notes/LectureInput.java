8e4801a09570ca446aaf6c73cd48983d8c07cd65
IrisVoongonUnicorns <irisvoong@gmail.com>
2026-09-18 18:01:31 +0000

    update

--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -6,7 +6,7 @@ import java.until.Scanner
 class LectureInput{
     public static void main(String args[]) {
         System.out.println ("Welcome to Yunju Heeroz");
-        Here's our menu:
+        System.out.println ("Here's our menu:");
         System.out.print("1. plum Cucumber stick - ");
         double item1price = $2.47;
         System.out.println(item1Price);
@@ -21,6 +21,26 @@ class LectureInput{
 
         System.out.println("How many plum Cucumber sticks would you like?");
         Scanner sc = new Scanner(System.in);
+        System.out.println("How much of plum cucumber stick do you want?");
+        int hanni = sc.nextInt();
+        System.out.println("plum cucumber stick- " + (item1price*hanni));
+
+        System.out.println("How much of galbi do you want?");
+        int hanni = sc.nextInt();
+        System.out.println("galbi- " + (item2price*hanni));
+
+        System.out.println("How much of tiramisu mini circle slice do you want?");
+        int hanni = sc.nextInt();
+        System.out.println("tiramisu mini circle slice- " + (item3price*hanni));
+
+        double finaltotal = (item1price*hanni) + (item2price*hanni) + (item3price*hanni);
+
+        System.out.println("How much would you like to tip?");
+        double tip = sc.nextDouble();
+
+        double math = (tip/100 * finaltotal);
+
+        System.out.println("Your total is: " + (finaltotal+tip));
 
 
 	}
--- Java/102_Exercises/09_Next5/base_code/starter.java
+++ Java/102_Exercises/09_Next5/base_code/starter.java
@@ -7,7 +7,39 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-
+		int nomber = 5;
+		System.out.println("please enter a number: " + nomber);
+		System.out.println("Here are the next 5 numbers!");
+		int nomber1 = (nomber+1);
+		int nomber2 = (nomber+2);
+		int nomber3 = (nomber+3);
+		int nomber4 = (nomber+4);
+		int nomber5 = (nomber+5);
+		System.out.print(nomber + ", ");
+		System.out.print(nomber1 + ", ");
+		System.out.print(nomber2 + ", ");
+		System.out.print(nomber3 + ", ");
+		System.out.print(nomber4 + ", ");
+		System.out.println(nomber5);
+		System.out.println("Here are the next 5 multiples of 5!");
+		int nimber1 = (nimber*1);
+		int nimber2 = (nimber*2);
+		int nimber3 = (nimber*3);
+		int nimber4 = (nimber*4);
+		int nimber5 = (nimber*5);
+		System.out.print(nimber * ", ");
+		System.out.print(nimber1 * ", ");
+		System.out.print(nimber2 * ", ");
+		System.out.print(nimber3 * ", ");
+		System.out.print(nimber4 * ", ");
+		System.out.println(nimber5);
+		System.out.println("here is 5 divided by 100!");
+		double moka = 0.05;
+		System.out.println(moka);
+		System.out.println("Here is " + moka2 + " divided by 10!");
+		double moka2 = 0.5;
+		System.out.println(moka2);
+		
 
 	}
 }
